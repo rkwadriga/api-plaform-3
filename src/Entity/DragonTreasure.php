d651d1d8711e0adb0dc5c90384680fe55c83192d
rkwadriga <dmitry.kushneriov@obreey-products.com>
2023-10-12 18:36:59 +0300

    * Allow to update the "user" resource fields on updating the "treasure" resource * Validate the "user" resource fields on updating the "treasure" resource

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -63,6 +63,7 @@ class DragonTreasure
     #[ORM\ManyToOne(inversedBy: 'dragonTreasures')]
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(['treasure:read', 'treasure:write'])]
+    #[Assert\Valid]
     private ?User $owner = null;
 
     #[ORM\Column(length: 255)]
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,7 +40,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $email = null;
 
     #[ORM\Column(length: 255, unique: true)]
-    #[Groups(['user:read', 'user:write', 'treasure:item:get'])]
+    #[Groups(['user:read', 'user:write', 'treasure:item:get', 'treasure:write'])]
     #[Assert\NotBlank]
     private ?string $username = null;
 
