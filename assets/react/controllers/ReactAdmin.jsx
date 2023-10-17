8788d84a9af0e0cf6dcc78a67b91e175a0658a44
rkwadriga <dmitry.kushneriov@obreey-products.com>
2023-10-17 16:51:02 +0300

    * Install the "encore" bundle * Install the "symfony/ux-react" bundle * Install the "doctrine/inflector" library * Install "api-platform/admin" js library * Install "babel/preset-react" js library * Enable strict_types for all PHP-classes * Create a "/admin" page witch start the api-platform React Admin

---
+++ assets/react/controllers/ReactAdmin.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { HydraAdmin } from '@api-platform/admin';
+
+export default (props) => {
+    <HydraAdmin entrypoint={props.entrypoint} />
+}
---
+++ src/Controller/AdminController.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function dashboard(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+}
